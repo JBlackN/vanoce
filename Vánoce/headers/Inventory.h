fc939cfd54e0be9b93688e6d0fd296e64a4ebe98
Petr Schmied <jblack@paworld.eu>
2015-12-22 14:44:34 +0100

    Documentation - Inventory

--- Vánoce/headers/Inventory.h
+++ Vánoce/headers/Inventory.h
@@ -15,34 +15,72 @@ using namespace std;
 #include "Fog.h"
 #include "Collision.h"
 
+/**
+ * Class describes inventory which can contain red, yellow and blue ornaments.
+ */
 class Inventory
 {
 public:
 	enum OrnamentType { red, yellow, blue };
 private:
-	Config * config;
+	Config * config; ///< Config provider.
 
 	Model * redOrnament;
 	Model * yellowOrnament;
 	Model * blueOrnament;
 
-	unsigned int nRedOrnaments;
-	unsigned int nYellowOrnaments;
-	unsigned int nBlueOrnaments;
+	unsigned int nRedOrnaments; ///< Count of red ornaments placed in inventory.
+	unsigned int nYellowOrnaments; ///< Count of yellow ornaments placed in inventory.
+	unsigned int nBlueOrnaments; ///< Count of blue ornaments placed in inventory.
 
-	map<OrnamentType, list<Object *>> placedOrnaments;
-	Collision * collision;
+	map<OrnamentType, list<Object *>> placedOrnaments; ///< Ornaments by color placed within the scene.
+	Collision * collision; ///< Ornament collision checker.
 public:
 	Inventory(Config * config, Model * redOrnament, Model * yellowOrnament, Model * blueOrnament);
 	~Inventory();
 
+	/**
+	 * Adds ornament to inventory.
+	 * @param type Ornament color (red, yellow, blue).
+	 * @param count How many ornaments to add.
+	 */
 	void insertOrnament(OrnamentType type, unsigned int count = 1);
-	void removeOrnament(OrnamentType type);
-	unsigned int ornamentCount(OrnamentType type);
-	void placeOrnament(OrnamentType type, Object * tree, glm::vec2 window_dimensions, int windowX, int windowY, Camera * camera);
-	void drawOrnaments(Camera * camera, map<string, Light *> lights, Fog * fog);
 
+	/**
+	 * Removes ornament from inventory.
+	 * @param type Ornament color (red, yellow, blue).
+	 */
+	void removeOrnament(OrnamentType type);
+
+	/**
+	 * Gets number of ornaments (of specified color) within inventory.
+	 * @param type Ornament color (red, yellow, blue).
+	 * @return Number of ornaments (of specified color) within inventory.
+	 */
+	unsigned int ornamentCount(OrnamentType type);
+
+	/**
+	 * Places ornament from inventory to the scene (on a specified tree).
+	 * @param type Ornament color (red, yellow, blue).
+	 * @param tree Tree to be decorated.
+	 * @param window_dimensions Current window dimensions.
+	 * @param windowX Mouse click x-coordinate.
+	 * @param windowY Mouse click y-coordinate.
+	 * @param camera Active camera.
+	 */
+	void placeOrnament(OrnamentType type, Object * tree, glm::vec2 window_dimensions, int windowX, int windowY, Camera * camera);
+	
+	void drawOrnaments(Camera * camera, map<string, Light *> lights, Fog * fog);
 private:
+	/**
+	 * Determines position within the scene where to place an ornament.
+	 * @param tree Tree to be decorated.
+	 * @param window_dimensions Current window dimensions.
+	 * @param windowX Mouse click x-coordinate.
+	 * @param windowY Mouse click y-coordinate.
+	 * @param camera Active camera.
+	 * @return Position.
+	 */
 	glm::vec3 findPosition(Object * tree, glm::vec2 window_dimensions, int windowX, int windowY, Camera * camera);
 };
 
