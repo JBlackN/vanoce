2066b02333b3dce86234abe155cad28bad24ac0a
Petr Schmied <jblack@paworld.eu>
2016-01-03 17:13:20 +0100

    Documentation - Texture

--- Vánoce/headers/Texture.h
+++ Vánoce/headers/Texture.h
@@ -6,11 +6,19 @@ using namespace std;
 
 #include "pgr.h"
 
+/**
+* Class holds one texture.
+*/
 class Texture
 {
 public:
 	GLuint texture;
 public:
+	/**
+	 * Loads new texture using specified path. Clamps texture to edge (both directions) if requested.
+	 * @param texPath Path to image file.
+	 * @param clamp Specifies whether clamp texture to edges or not.
+	 */
 	Texture(string texPath, bool clamp = false);
 	~Texture();
 };
