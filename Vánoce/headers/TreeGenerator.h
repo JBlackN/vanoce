e0aff44918f84f21794050a7c71cea21ecadc368
Petr Schmied <jblack@paworld.eu>
2016-01-03 17:31:26 +0100

    Documentation - TreeGenerator

--- Vánoce/headers/TreeGenerator.h
+++ Vánoce/headers/TreeGenerator.h
@@ -14,25 +14,65 @@ using namespace std;
 #include "Light.h"
 #include "Fog.h"
 
+/**
+* Class provides random tree generation.
+*/
 class TreeGenerator
 {
 private:
-	Config * config;
-	enum MapSegmentStatus : bool { tree = true, treeSurroundings = true, home = true, nothing = false };
+	Config * config; ///< Config provider.
+
+	/**
+	 * Enumeration describes states of one scene map segment.
+	 */
+	enum MapSegmentStatus : bool
+	{
+		tree = true, ///< Segment contains a tree.
+		treeSurroundings = true, ///< Segment is next to another segment which contains a tree.
+		home = true, ///< Segment lies inside home (no tree generation there).
+		nothing = false ///< Segment can be used to place a new tree.
+	};
 
 	Model * treeModel;
-	MapSegmentStatus ** sceneMap;
-	int sceneMapDimension;
+	MapSegmentStatus ** sceneMap; ///< Scene map divided to segments.
+	int sceneMapDimension; ///< Number of segments (map edge).
 public:
-	vector<Object *> trees;
+	vector<Object *> trees; ///< Generated and placed trees.
 public:
+	/**
+	 * Creates new tree generator.
+	 * @param config Config provider.
+	 * @param treeModel Tree model.
+	 * @param mapDimension Map QUADRANT edge dimension.
+	 */
 	TreeGenerator(Config * config, Model * treeModel, int mapDimension);
 	~TreeGenerator();
 
+	/**
+	 * Generates specified number of trees.
+	 * @param count Number of trees to generate.
+	 */
 	void generateTrees(int count);
+
+	/**
+	 * Draws generated trees.
+	 * @param camera Camera to use.
+	 * @param lights Lights to use.
+	 * @param fog Fog to use.
+	 * @param firstID First drawn tree's ID (used for later tree picking).
+	 */
 	void drawTrees(Camera * camera, map<string, Light *> lights, Fog * fog, int firstID);
 private:
+	/**
+	 * Inits segment map of a scene.
+	 */
 	void initMap();
+
+	/**
+	 * Marks segments surrounging placed tree in a scene map.
+	 * @param randomX X-coordinate of a placed tree.
+	 * @param randomZ Z-coordinate of a placed tree.
+	 */
 	void markTreeSurroundings(int randomX, int randomZ);
 };
 
